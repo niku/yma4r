2076d2b1db81bdd40ff710fcba7cd40a9329594c
niku <niku@niku.name>
2009-02-12 22:06:51 +0900

    change YmaParser from accesser to method

--- lib/yma_parser.rb
+++ lib/yma_parser.rb
@@ -1,15 +1,21 @@
 class YmaParser
   require 'rexml/document'
-  attr_reader :xml, :total_count, :filtered_count, :word_list
+  attr_reader :xml
   def initialize(xml)
     @xml = REXML::Document.new(xml)
-    @total_count = @xml.elements['//total_count'].text.to_i
-    @filtered_count = @xml.elements['//filtered_count'].text.to_i
-    @word_list = @xml.elements['//word_list'].map do |word|
-      { :surface=>word.elements['surface'].text,
-        :reading=>word.elements['reading'].text,
-        :pos=>word.elements['pos'].text
-      }
+  end
+
+  def total_count
+    @xml.elements['//total_count'].text.to_i
+  end
+
+  def filtered_count
+    @xml.elements['//filtered_count'].text.to_i
+  end
+
+  def word_list
+    @xml.elements['//word_list'].map do |word|
+      {:surface=>word.elements['surface'].text, :reading=>word.elements['reading'].text, :pos=>word.elements['pos'].text}
     end
   end
 end
