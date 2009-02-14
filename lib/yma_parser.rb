3a53e30842400047210b01adedb406fdc6021ecf
niku <niku@niku.name>
2009-02-15 00:47:05 +0900

    fix nil element cause error

--- lib/yma_parser.rb
+++ lib/yma_parser.rb
@@ -1,6 +1,7 @@
 class YmaParser
   require 'rexml/document'
   attr_reader :xml
+
   def initialize(xml)
     @xml = REXML::Document.new(xml)
   end
@@ -14,8 +15,13 @@ class YmaParser
   end
 
   def word_list
+    responses = [:surface, :reading, :pos, :baseform, :feature]
     @xml.elements['//word_list'].map do |word|
-      {:surface=>word.elements['surface'].text, :reading=>word.elements['reading'].text, :pos=>word.elements['pos'].text}
+      hash = {}
+      responses.each do |name|
+        hash[name] = word.elements[name.to_s] ? word.elements[name.to_s].text : nil 
+      end
+      hash.delete_if{|key,val| val==nil}
     end
   end
 end
